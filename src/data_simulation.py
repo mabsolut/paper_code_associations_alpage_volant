99eb996927b98d4a8a692fcfb1f32a49b1053208
mabsolut <mathieu.absolut_de_la_gastine@ens-lyon.fr>
2025-07-01 17:20:40 +0200

    Add files via upload

---
+++ src/data_simulation.py
@@ -0,0 +1,165 @@
+import json
+import multiprocessing as mp
+import random
+from collections import Counter, defaultdict
+
+import numpy as np
+import pandas as pd
+from association_detection import detect_species_associations
+
+
+def simulate_alpha_error(
+    df, treatment, nbplots: int = 30, n_iter: int = 1000, n_cores: int = 12
+):
+    """
+    Performs simulation-based estimation of alpha error under the null hypothesis,
+    by randomizing species–pinpoint assignments within plots.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Cleaned dataset containing columns like 'Species', 'pinpoint', etc.
+    treatment : str
+        Site_Treatment to analyze (e.g., 'G_CP', 'L_TP', etc.).
+    nbplots : int
+        Number of plots to include in each simulation.
+    n_iter : int
+        Number of random simulations to perform.
+    n_cores : int
+        Number of processes to use for parallel computation.
+    """
+    subdf = df[df["Site_Treatment"] == treatment]
+    grouped = list(subdf.groupby(["Year", "Site_Treatment", "Subplot", "Replicate"]))
+    all_subdfs = []
+
+    for _ in range(n_iter):
+        random.shuffle(grouped)
+        simdf = []
+        for _, subsimdf in grouped[:nbplots]:
+            total_pinpoint = subsimdf["pinpoint"].nunique()
+            abundances = dict(Counter(subsimdf["Species"]))
+            randomized_rows = []
+
+            for species, abundance in abundances.items():
+                tirage = np.random.choice(
+                    range(total_pinpoint), abundance, replace=False
+                )
+                rows = (
+                    subsimdf[subsimdf["Species"] == species]
+                    .copy()
+                    .reset_index(drop=True)
+                )
+                for i, pinpoint in enumerate(tirage):
+                    row = rows.iloc[i].copy()
+                    row["pinpoint"] = pinpoint
+                    randomized_rows.append(row)
+
+            randomized_df = pd.DataFrame(randomized_rows)
+            simdf.append(randomized_df)
+
+        all_subdfs.append(pd.concat(simdf, ignore_index=True))
+
+    with mp.Pool(n_cores) as pool:
+        res_list = pool.map(detect_species_associations, all_subdfs)
+
+    # Save
+    save_path = f"/home/alpage_volant/alpage_volant_interaction/save_json/json_alpha_error_{treatment}.json"
+    with open(save_path, "w") as f:
+        json.dump(res_list, f)
+    return res_list
+
+
+def report_alpha_error(res_list, treatment):
+    """
+    Loads alpha error simulation results and prints average number of false positive links.
+
+    Parameters
+    ----------
+    treatment : str
+        Site_Treatment identifier (e.g., 'G_CP', 'L_TP').
+    """
+    print(
+        "Alpha error of positive links",
+        treatment,
+        ":",
+        np.mean([len(res_dict["over"]) for res_dict in res_list]),
+    )
+
+    print(
+        "Alpha error of negative links",
+        treatment,
+        ":",
+        np.mean([len(res_dict["under"]) for res_dict in res_list]),
+    )
+
+
+def compute_impact_plots_simulation(df, treatment):
+    """
+    Estimates the accumulation of species associations (links) as a function of the number of sampling plots,
+    under a null model where pinpoint-level spatial structure is randomized while maintaining species abundances.
+
+    This simulates the expected increase in co-occurrences due to sample size alone.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Preprocessed input dataset including columns: 'Year', 'Site_Treatment', 'Subplot', 'Replicate',
+        'Species', and 'pinpoint'.
+    treatment : str
+        Name of the treatment to consider (e.g., 'G_CP', 'L_TP').
+
+    Returns
+    -------
+    result : dict
+        Dictionary where keys are the number of used plots, and values are lists of
+        detected associations (output of detect_species_associations).
+    """
+    subdf = df[df["Site_Treatment"] == treatment]
+
+    result = defaultdict(list)
+
+    grouped = list(subdf.groupby(["Year", "Site_Treatment", "Subplot", "Replicate"]))
+    nb_plots = len(grouped)
+    all_subdfs = []
+    ns = []
+
+    for n in range(1, nb_plots + 1):
+        random.shuffle(grouped)
+        for i in range(0, len(grouped) - n + 1, n):
+            selected_groups = [group[1] for group in grouped[i : i + n]]
+            simdf = []
+            for subsimdf in selected_groups:
+                total_pinpoint = subsimdf["pinpoint"].nunique()
+                abondances = dict(Counter(subsimdf["Species"]))
+                randomized_rows = []
+                for specie, abondance in abondances.items():
+                    tirage = np.random.choice(
+                        range(total_pinpoint), abondance, replace=False
+                    )
+                    matching_rows = (
+                        subsimdf[subsimdf["Species"] == specie]
+                        .copy()
+                        .reset_index(drop=True)
+                    )
+                    for i, pinpoint in enumerate(tirage):
+                        row = matching_rows.iloc[i].copy()
+                        row["pinpoint"] = pinpoint
+                        randomized_rows.append(row)
+                randomized_df = pd.DataFrame(randomized_rows)
+                simdf.append(randomized_df)
+            all_subdfs.append(pd.concat(simdf, ignore_index=True))
+            ns.append(n)
+
+    with mp.Pool(12) as pool:
+        res_list = pool.map(detect_species_associations, all_subdfs)
+
+    for n, res_dict in zip(ns, res_list):
+        result[n].append(res_dict)
+
+    with open(
+        f"/home/alpage_volant/alpage_volant_interaction/save_json/json_impact_plots_simulation_{treatment}.json",
+        "w",
+    ) as f:
+        json.dump(result, f)
+
+    return result
---
+++ src/network_visualization.py
@@ -0,0 +1,340 @@
+import json
+import multiprocessing as mp
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+from association_detection import detect_species_associations
+from data_preprocessing import compute_DCi
+from matplotlib.lines import Line2D
+from scipy.stats import mannwhitneyu
+
+
+def graph_creation(df, origin):
+    """
+    Visualisation of species spatial association networks across treatments.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Data frame containing species observations with columns 'Site_Treatment', 'Subplot', and 'Species'.
+    origin : dict
+        Dictionary mapping species to colors.
+
+    Returns
+    -------
+    degrees : dict
+        Species degree (number of connections) per treatment.
+    DCis : dict
+        Dominance Candidate Index per treatment.
+    """
+    treatments = ["G_CP", "L_TP", "L_CP"]
+    subdf = df[df["Site_Treatment"].isin(treatments)]
+    # Keep only one subplot by treatment (alpine, alpine warmed and subalpine) to be able to compare them after
+    subdf = subdf[(subdf["Subplot"] == "B") | (subdf["Subplot"] == "0")]
+
+    # === Detect spatial associations ===
+    subdf_grouped = subdf.groupby(["Site_Treatment"])
+    with mp.Pool(12) as pool:
+        res_list = pool.map(
+            detect_species_associations,
+            [group for _, group in subdf_grouped],
+        )
+    result = {
+        treatment: {
+            "Site_Treatment": treatment,
+            "res_dict": res_dict,
+        }
+        for treatment, res_dict in zip(subdf_grouped.indices.keys(), res_list)
+    }
+
+    min_pval = min(
+        [
+            min(list(res["over"].values()) + list(res["under"].values()), default=1)
+            for res in (d["res_dict"] for d in result.values())
+        ]
+    )
+
+    def normalize_weight(value, min_value=min_pval, min_weight=0.2, max_weight=1e5):
+        return min_weight + (max_weight - min_weight) * (min_value / value)
+
+    # === Create reference layout for alpine and alpine warmed networks ===
+    ref_graph = nx.Graph()
+    for t in ["G_CP", "L_TP"]:
+        for edge_type in ["under", "over"]:
+            for link in result[t]["res_dict"][edge_type].keys():
+                sp1, sp2 = link.split("|")
+                ref_graph.add_edge(sp1, sp2, weight=1)
+    pos_G = nx.kamada_kawai_layout(ref_graph)
+
+    degrees, DCis = {}, {}
+    print("=== Network summary ===")
+    for t in treatments:
+        _, ax = plt.subplots(figsize=(4, 4))
+        subdf_t = subdf[subdf["Site_Treatment"] == t]
+        DCi = compute_DCi(subdf_t)
+        DCis[t] = DCi
+        res = result[t]["res_dict"].copy()
+
+        if t == "L_CP":
+            edge_sets, edge_colors = [res["under"], res["over"]], ["red", "blue"]
+        else:
+            ref_key = "L_TP" if t == "G_CP" else "G_CP"
+            res_ref = result[ref_key]["res_dict"].copy()
+
+            # selection of links common to the alpine and alpine warmed networks
+            shared_under = {
+                k: v for k, v in res_ref["under"].items() if k in res["under"].keys()
+            }
+            # selection of links unique to the alpine or alpine warmed network
+            unique_under = {
+                k: v for k, v in res["under"].items() if k not in shared_under.keys()
+            }
+
+            edge_sets = [unique_under, res["over"], shared_under]
+            edge_colors = (
+                ["#BF00FF", "blue", "#6e6e6eff"]
+                if t == "G_CP"
+                else ["#fbaf00", "blue", "#6e6e6eff"]
+            )
+
+        # === Build graph ===
+        g = nx.Graph()
+        for color, edges in zip(edge_colors, edge_sets):
+            g.add_weighted_edges_from(
+                [
+                    (
+                        pair.split("|")[0],
+                        pair.split("|")[1],
+                        -np.log10(normalize_weight(pval)),
+                    )
+                    for pair, pval in edges.items()
+                ],
+                weight="pval",
+                color=color,
+            )
+
+        node_sizes = [DCi.get(node, 0) * 100 for node in g.nodes()]
+
+        if t == "L_CP":
+            node_colors = [origin.get(node) for node in g.nodes()]
+            pos = nx.spring_layout(g, seed=42)
+        else:
+            nodes_ref = {
+                node for edge in res_ref["under"].keys() for node in edge.split("|")
+            }
+            node_colors = [
+                (
+                    "#6e6e6eff"
+                    if node in nodes_ref
+                    else "#BF00FF" if t == "G_CP" else "#fbaf00"
+                )
+                for node in g.nodes()
+            ]
+            pos = pos_G
+            x, y = pos["Pla mari"]
+            pos["Pla mari"] = (x, y - 0.03)
+
+        nx.draw_networkx_nodes(g, pos, node_color=node_colors, node_size=node_sizes)
+        nx.draw_networkx_edges(
+            g,
+            pos,
+            edge_color=nx.get_edge_attributes(g, "color").values(),
+            width=list(nx.get_edge_attributes(g, "pval").values()),
+        )
+        nx.draw_networkx_labels(g, pos, ax=ax, font_size=6, font_color="black")
+        ax.set_title(t)
+        ax.axis("off")
+
+        # === Plot absent species ===
+        absent_sp = sorted(
+            set(subdf_t["Species"].unique()) - set(g.nodes()),
+            key=lambda sp: DCi.get(sp, 0),
+            reverse=True,
+        )
+        for k, sp in enumerate(absent_sp):
+            x = -1 + (k % int(len(absent_sp) / 3)) * 0.08
+            y = -1 - 0.08 * (k // int(len(absent_sp) / 3))
+            color = origin.get(sp) if t == "L_CP" else "black"
+            ax.scatter(x, y, s=DCi.get(sp) * 100, color=color)
+
+        order = len(g.nodes())
+        size = len(g.edges())
+        print(
+            t,
+            "- Order = ",
+            order,
+            "; Size = ",
+            size,
+            "; Connectance = ",
+            size / (order * (order - 1) / 2),
+        )
+        degrees[t] = dict(g.degree())
+
+        plt.savefig(
+            f"/home/alpage_volant/alpage_volant_interaction/save_result/graphs/graphs_{t}.svg",
+            format="SVG",
+            dpi=300,
+        )
+    return degrees, DCis
+
+
+def graph_creation_common(df, degrees):
+    """
+    Visualizes spatial association links shared between the alpine control (G_CP)
+    and alpine warmed (L_TP) communities, comparing interaction strength and
+    detecting significant shifts represented in a combined network.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Data with 'Site_Treatment' and species observations.
+    degrees : dict
+        Dictionary of species degrees per treatment from previous analysis.
+
+    Returns
+    -------
+    None
+    """
+    # Compute associations from pooled data
+    pooled_df = df[df["Site_Treatment"].isin(["G_CP", "L_TP"])]
+    res = detect_species_associations(pooled_df, plot_impact=True)
+    with open(
+        "/home/alpage_volant/alpage_volant_interaction/save_json/json_common.json", "w"
+    ) as f:
+        json.dump(res, f)
+
+    # Compare negative association strengths between alpine and alpine warmed communities
+    importance = res["importance_under"]
+
+    stronger_a, stronger_w, unchanged = {}, {}, {}
+    only_a, only_w = {}, {}
+
+    for pair, pourcents in importance.items():
+        vals_a = [
+            p
+            for plot, p in pourcents.items()
+            if "_".join(plot.split("_")[1:3]) == "G_CP"
+        ]
+        vals_w = [
+            p
+            for plot, p in pourcents.items()
+            if "_".join(plot.split("_")[1:3]) == "L_TP"
+        ]
+        if vals_a and vals_w:
+            _, pval_less = mannwhitneyu(vals_a, vals_w, alternative="less")
+            _, pval_greater = mannwhitneyu(vals_a, vals_w, alternative="greater")
+            if pval_less <= 0.05:
+                stronger_w[pair] = res["under"][pair]
+            elif pval_greater <= 0.05:
+                stronger_a[pair] = res["under"][pair]
+            else:
+                unchanged[pair] = res["under"][pair]
+        elif vals_a:
+            only_a[pair] = res["under"][pair]
+        else:
+            only_w[pair] = res["under"][pair]
+
+    # Network building
+    min_pval = min(list(res["under"].values()))
+
+    def normalize_weight(value, min_value=min_pval, min_weight=0.2, max_weight=1e5):
+        return min_weight + (max_weight - min_weight) * (min_value / value)
+
+    g = nx.Graph()
+
+    color_map = {
+        "unchanged": "#e6e6e6",
+        "stronger_a": "#BF00FF",
+        "stronger_w": "#fbaf00",
+        "only_w": "yellow",
+        "only_a": "grey",
+    }
+
+    for group, color in zip(
+        [unchanged, stronger_a, stronger_w, only_w, only_a],
+        color_map.values(),
+    ):
+        g.add_weighted_edges_from(
+            [
+                (
+                    pair.split("|")[0],
+                    pair.split("|")[1],
+                    -np.log10(normalize_weight(pval)),
+                )
+                for pair, pval in group.items()
+            ],
+            weight="pval",
+            color=color,
+        )
+
+    # Compute node properties
+    DCi = compute_DCi(pooled_df)
+    node_sizes, node_colors = [], []
+    for node in g.nodes():
+        if node in degrees["G_CP"].keys() and node in degrees["L_TP"].keys():
+            node_colors.append("grey")
+        elif node in degrees["G_CP"].keys():
+            node_colors.append("#BF00FF")
+        elif node in degrees["L_TP"].keys():
+            node_colors.append("#fbaf00")
+        else:
+            node_colors.append("black")
+        node_sizes.append(DCi.get(node) * 100)
+    pos = nx.spring_layout(g, seed=42, k=0.4, iterations=200)
+
+    # Plot of the network
+    _, ax = plt.subplots()
+    nx.draw_networkx_nodes(
+        g, pos, node_color=node_colors, node_size=node_sizes, linewidths=1
+    )
+    nx.draw_networkx_edges(
+        g,
+        pos,
+        edge_color=nx.get_edge_attributes(g, "color").values(),
+        width=list(nx.get_edge_attributes(g, "pval").values()),
+    )
+    nx.draw_networkx_labels(g, pos, font_size=6, font_color="black", ax=ax)
+
+    # Adding of a pie chart summarizing changes
+    labels = ["Decreased with warming", "Increased with warming"]
+    sizes = [len(stronger_a), len(stronger_w)]
+    colors_pie = [color_map["stronger_a"], color_map["stronger_w"]]
+    inset_ax = ax.inset_axes([0.75, 0.05, 0.2, 0.2])
+    inset_ax.pie(
+        sizes,
+        labels=labels,
+        colors=colors_pie,
+        startangle=90,
+        autopct="%1.1f%%",
+        textprops={"fontsize": 6},
+    )
+    inset_ax.set_title("Link bias", fontsize=6)
+
+    # Legend
+    legend_elements = [
+        Line2D(
+            [0], [0], color="#BF00FF", label="Increased with warming", linestyle="-"
+        ),
+        Line2D(
+            [0], [0], color="#fbaf00", label="Decreased with warming", linestyle="-"
+        ),
+        Line2D([0], [0], color="yellow", label="Appeared with warming", linestyle="-"),
+        Line2D([0], [0], color="grey", label="Disappeared with warming", linestyle="-"),
+        Line2D(
+            [0],
+            [0],
+            color="#e6e6e6",
+            label="Did not change with warming",
+            linestyle="-",
+        ),
+    ]
+    plt.legend(handles=legend_elements, loc=(1, 1))
+
+    ax.axis("off")
+    plt.tight_layout()
+    plt.savefig(
+        f"/home/alpage_volant/alpage_volant_interaction/save_result/graphs/graphs_common.svg",
+        format="SVG",
+        dpi=300,
+    )
